d484bc012e98194c385f0f2e44373951b5637200
theonlyoxtale <theonlyoxtale@gmail.com>
2017-08-05 21:49:55 -0400

    added the code for forward and backwards moment and added a test gun asset

---
+++ Game_0/Assets/_Scripts/HelloWorld.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+    int timerTest = 5;
+
+	// Use this for initialization
+	void Start () {
+
+        changeColor(timerTest);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.black;
+        }
+
+	}
+
+    public void changeColor(int number)
+    {
+        for (int myNumber = 0; myNumber < number; myNumber++)
+        {
+            this.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+            Debug.Log(myNumber);
+        }
+    }
+}
---
+++ Game_0/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //variable for player speed
+    public float speed = 5f;
+
+    //variable for holding the player rigidbody
+    private Rigidbody2D rig;
+
+	// Use this for initialization
+	void Start () {
+
+        //grabs the rigidbody from the player object
+        rig = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //variable for moving player left and right
+        float hAxis = Input.GetAxis("Horizontal");
+        //variable for moving player forward and backward
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, vAxis, 0)*speed*Time.deltaTime;
+        rig.MovePosition(transform.position + movement);
+		
+	}
+}
