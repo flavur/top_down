d810f720639f7414ee8fdcaa586551bd446ff0bf
theonlyoxtale <theonlyoxtale@gmail.com>
2017-08-07 21:35:32 -0400

    added more code for firing the bullet
    added code for shooting and added a new bullet png that needs to be
    redone

--- Game_0/Assets/_Scripts/PlayerController.cs
+++ Game_0/Assets/_Scripts/PlayerController.cs
@@ -10,6 +10,17 @@ public class PlayerController : MonoBehaviour {
     //variable for holding the player rigidbody
     private Rigidbody2D rig;
 
+    //location where the bullet will fire from
+    public GameObject bulletPoint;
+
+    //holds the prefab for the builet
+    public GameObject bullet;
+
+    //spped of the bullet
+    public float bulletForwardForce;
+
+    public float bulletDestroyTime = 5f;
+
 	// Use this for initialization
 	void Start () {
 
@@ -28,6 +39,21 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 movement = new Vector3(hAxis, vAxis, 0)*speed*Time.deltaTime;
         rig.MovePosition(transform.position + movement);
-		
-	}
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Bullet instantiation
+            GameObject tempBulletHolder = Instantiate(bullet,bulletPoint.transform.position,bulletPoint.transform.rotation);
+            tempBulletHolder.transform.Rotate(Vector3.left * 90);
+
+            //Retrieve the rigidbody component from the instantiated bullet and control it
+            Rigidbody tempRigidbody = tempBulletHolder.GetComponent<Rigidbody>();
+            //Tell the bullet object to be pushed forward
+            tempRigidbody.AddForce(transform.forward * bulletForwardForce);
+
+            //Basic cleanup, sets the bullet to be destroyed after a certain amount of time
+            Destroy(tempBulletHolder, bulletDestroyTime);
+        }
+
+    }
 }
